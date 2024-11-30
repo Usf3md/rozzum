60f4608c9fa7b1aaadcffd98014151bbc30199f2
Youssef Emad <usf3md@gmail.com>
2024-11-30 07:09:04 +0200

    add things

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
@@ -9,6 +8,8 @@ import { LoginForm } from "./components/LoginForm";
 import { useRouter } from "next/navigation";
 import { useUser } from "../context/UserContext";
 import { useEffect } from "react";
+import gif from "@/public/rozzum.gif";
+import Logo from "@/components/Logo";
 
 export default function AuthenticationPage() {
   const router = useRouter();
@@ -24,53 +25,42 @@ export default function AuthenticationPage() {
       .then(() => router.push("/"));
   }, []);
   return (
-    <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="h-screen flex items-center w-full">
+      <div
+        className="h-full flex-col bg-muted p-10 lg:flex w-1/2"
+        style={{
+          backgroundImage: `url('${gif.src}')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "50% 0 ",
+        }}
+      ></div>
+      <div className="p-8 w-1/2 relative h-screen flex items-center justify-center">
+        <div className="absolute left-8 top-8 flex h-[52px] items-center p-4 gap-2">
+          <Logo size={28} />
+          <h1 className="text-xl font-bold">Rozzum</h1>
+        </div>
         <Link
           href="/signup"
           className={cn(
             buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
+            "absolute right-8 top-8"
           )}
         >
           Sign up
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            Rozzum
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Log in with your account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your credentials below to log in
+            </p>
           </div>
-        </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Log in with your account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your credentials below to log in
-              </p>
-            </div>
-            <LoginForm />
-          </div>
+          <LoginForm />
         </div>
       </div>
-    </>
+    </div>
   );
 }
